4ec3c9e1cf9733b2b3dea49bc979c2254abb661f
Chris Low <chrislow360@gmail.com>
2018-10-20 23:34:15 -0400

    delete all auth and added documentation

--- server.js
+++ server.js
@@ -7,17 +7,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-
 /** port for heroku and localhost */
-// const MongoClient = require('mongodb').MongoClient;
-// const url = "mongodb+srv://boilermake2018:boilermake2018@cluster0-xubmh.mongodb.net/test?retryWrites=true"
-// const dbName = 'boilermake2018';
-
-// MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-//     console.log("Connected to server.");
-//     const db = client.db(dbName);
-// });
+const port = process.env.PORT || 5000;
 
 /** user database */
 let users = [
@@ -36,11 +27,23 @@ let joblist = [
     {title: "Babysitter", description: "Care for young children", name: "Sarah", details: "Weekends 9am - 3pm", contact: "7654330899"},
     {title: "Carpool", description: "To and fro Purdue and The Cottage", name: "Kristen", details: "Weekdays 9am, 3pm", contact: "sarah@purdue.edu"}
 ];
-// remove when done
-app.get('/', (req, res) => {
-    res.status(200).send({message: "Working"});
+
+/** GET: joblist */
+app.get('/joblist', (req, res) => {
+    res.status(200).send(joblist);
 });
 
+/** GET: search */
+app.get('/search', (req, res) => {
+    let title = req.query.title;
+
+    let job = joblist.filter(jobs => {
+        return jobs.title === title;
+    });
+    res.status(200).send(job);
+});
+
+/** POST: login */
 app.post('/login', (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
@@ -52,16 +55,11 @@ app.post('/login', (req, res) => {
     if (validUser.length === 0) {
         res.status(404).send({message: "User not found."});
     } else {
-        let token = "";
-
-        for(let i = 0; i < 4; i++) {
-            token += Math.random().toString(36).substring(2, 7) + "";
-        }
-        res.setHeader('Set-Authorization', token);
         res.status(200).send({message: "Logged in!"});
     }
 });
 
+/** POST: signup */
 app.post('/signup', (req, res) => {
     let firstName = req.body.firstName;
     let lastName = req.body.lastName;
@@ -84,45 +82,25 @@ app.post('/signup', (req, res) => {
     res.send(users);
 });
 
-app.get('/joblist', (req, res) => {
-    res.status(200).send(joblist);
-});
-
+/** POST: new-job */
 app.post('/new-job', (req, res) => {
-    if (!req.headers.authorization || req.headers.authorization === "null") {
-        res.status(401).send({message: "Unauthorized."});
-    } else {
-        let title = req.body.title;
-        let desc = req.body.description;
-        let details = req.body.details;
-        let contact = req.body.contact;
+    let title = req.body.title;
+    let desc = req.body.description;
+    let details = req.body.details;
+    let contact = req.body.contact;
 
-        let newJob = {
-            id: items.length,
-            title: title,
-            description: desc,
-            details: details,
-            name: name,
-            contact: contact
-        };
-        joblist.push(newJob);
-        res.status(200).send(joblist);
-    }
-});
-
-app.get('/search', (req, res) => {
-    if (!req.headers.authorization || req.headers.authorization == "null") {
-        res.status(401).send({message: "Unauthorized."});
-    } else {
-        let title = req.query.title;
-
-        let job = joblist.filter(jobs => {
-            return jobs.title === title;
-        });
-        res.status(200).send(job);
-    }
+    let newJob = {
+        id: items.length,
+        title: title,
+        description: desc,
+        details: details,
+        name: name,
+        contact: contact
+    };
+    joblist.push(newJob);
+    res.status(200).send(joblist);
 });
 
 app.listen(port, () => {
     console.log("Server is running.");
-})
+});
